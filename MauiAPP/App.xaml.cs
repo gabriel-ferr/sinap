0334f2e1a9976ca98fb754bf170f0bed8ea3784c
Gabriel Ferreira <83837463+gabriel-ferr@users.noreply.github.com>
2023-10-11 15:23:41 -0300

    Create MAUI project.
    Create the MAUI project with C#.

---
+++ MauiAPP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAPP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAPP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
